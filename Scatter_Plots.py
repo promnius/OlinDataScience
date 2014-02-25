e52c91e61f2f0953f0d444fc0535aa6958dac520
promnius <Kyle.Mayer@students.olin.edu>
2014-02-24 21:54:12 -0500

    Added Comments to Scatter_Plots. Layout and Efficiency still need work.

--- Scatter_Plots.py
+++ Scatter_Plots.py
@@ -1,18 +1,4 @@
-"""
-CURRENT KNOWN ISSUES
-code for testing record recieved latency has a bug.
 
-We noticed that many of the machines seem to have discrepencies with data that should be constant but isn't. one 
-commonly noticed thing was the install date changes. this script identifies which machines have a constant install
-date.
-
-It checks all sorts of discrepencies, including:
-install dates not consistent
-created at not close to equal recieved at (latency)
-
-Planned Updates:
-the function split_up_machine_events should return a list of machines where the machines are sorted alphabeticlly by sn. or at least somehow sorted.
-"""
 
 import cyb_records
 import time, datetime
@@ -22,6 +8,8 @@ import matplotlib.pyplot as plt
 import checking_install_date_consistency as prep
 import clean_events
 
+
+"""pulls out the last chronological record, in the event that the records are NOT sorted by date."""
 def latest_record(machine):
     #print(str(machine[0].sn) + str(len(machine)) + "entries.")
     latest_timestamp = int(time.mktime(machine[0].received_at.timetuple()))
@@ -36,7 +24,17 @@ def latest_record(machine):
             #print "New timestamp is latest."
     return latest_record
         
+        
+"""walks through the process of pulling out each variable that is desired from the respective table, and adding them
+all to an intuitive list, then splitting the list up by desired factors (such as site code), and plotting the data.
+this function is a little long, and maybe should be split up into smaller ones, but outlines one method of drawing data
+from all the databases and combining them in a usable fashion. note that this function is VERY innefficient- it uses
+linear searches for most of the lookups, and has a few loops that are O(N^2). These can be made more efficient if 
+this code ends up being useful, but the order is still irrelevant for the small size of the data bases (Feb 24)"""
 def main():
+    
+    # so that you don't have to type in passwords and usernames every time you connect- since you connect separately
+    # for each table.
     myusername = raw_input("Please enter your username: ")
     mypassword = raw_input("Please enter your password: ")
     
@@ -44,11 +42,11 @@ def main():
     evs.ReadRecords(username = myusername, password = mypassword)
     machines_events_list = prep.split_up_machine_events(evs.records)
     
-    current_machines = []
-    # format for a machine in current machines:
-    # [sn(str), up_time(int), site_code(int), ]
+    current_machines = [] # list of all the attributes we are interested in for each machine, for the most current entry.
+    # format for a machine in current_machines:
+    # [sn(str), up_time(int), site_code(int), number of errors]
     
-    # grab sn, up time from the stats table
+    # grab sn, up time from the stats table, add them to the current_machines list
     for machine in machines_events_list:
         new_list = []
         if "" in str(machine[0].sn): # optional filtering of which records are collected.
@@ -89,6 +87,7 @@ def main():
             errors.append([str(record.sn), 1])
     #print errors
     
+    # taking these numbers of errors and adding them to the current_machines list
     for error in errors:
         for machine in current_machines:
             if machine[0] == error[0]:
@@ -101,7 +100,8 @@ def main():
             
     #print current_machines
     
-    # extract the variables
+    # extract the variables into separate vectors for plotting, based on what we want to plot them by.
+    # mainly split up by site code- ignoring sites that don't exist.
     real_up_time = []
     real_error_count = []
     fake_up_time = []
@@ -119,8 +119,47 @@ def main():
     print("Up times: " + str(real_up_time))
     print("Error Counts: " + str(real_error_count))
     
-    plt.scatter(real_up_time, real_error_count, s = 100)
+    # preparing to print with color codes for site codes. splitting up main vector again.
+    up_time9 = []
+    up_time10 = []
+    up_time11 = []
+    up_time12 = []
+    error_count9 = []
+    error_count10 = []
+    error_count11 = []
+    error_count12 = []
+    for machine in current_machines:
+        site_code = machine[2]
+        if site_code == 9:
+            up_time9.append(machine[1])
+            error_count9.append(machine[3])
+        elif site_code == 10:
+            up_time10.append(machine[1])
+            error_count10.append(machine[3])
+        elif site_code == 11:
+            up_time11.append(machine[1])
+            error_count11.append(machine[3])
+        elif site_code == 12:
+            up_time12.append(machine[1])
+            error_count12.append(machine[3])
+        else:
+            pass # fake machine
+    
+    # plotting
+    #plt.scatter(real_up_time, real_error_count, s = 100)
     #plt.scatter(fake_up_time, fake_error_count,  s = 20)
+
+    plt.scatter(up_time9, error_count9, facecolor= 'r', s = 300, label= 'site code 9')
+    plt.scatter(up_time10, error_count10, facecolor= 'b', s = 300, label = 'site code 10')
+    plt.scatter(up_time11, error_count11, facecolor= 'g', s = 300, label = 'site code 11')
+    plt.scatter(up_time12, error_count12, facecolor= 'k', s = 300, label = 'site code 12')
+    
+    plt.legend()
+    plt.xlabel('Distance (Meters?)', fontsize = 24)
+    plt.ylabel('Number of Errors', fontsize = 24)
+    plt.title('Errors vs. Distance, by Machine and Site', fontsize = 30)
+    plt.xlim([0, 100000000])
+    plt.ylim([0,60])
     plt.show()
     
 if __name__ == '__main__':
